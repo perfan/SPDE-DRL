4d753dc54b100e3990aea9dded5faf5ac7454641
faraz khoshbakhtian <39635965+faraz2023@users.noreply.github.com>
2021-04-04 12:08:16 -0400

    Cuds added

--- DDPG.py
+++ DDPG.py
@@ -40,7 +40,7 @@ class CriticNetwork(nn.Module):
         #self.q.bias.data.uniform_(-f3, f3)
 
         self.optimizer = optim.Adam(self.parameters(), lr=beta)
-        self.device = T.device('cpu')
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cuda:1')
 
         self.to(self.device)
 
@@ -101,7 +101,7 @@ class ActorNetwork(nn.Module):
         #self.mu.bias.data.uniform_(-f3, f3)
 
         self.optimizer = optim.Adam(self.parameters(), lr=alpha)
-        self.device = T.device('cpu')
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cuda:1')
 
         self.to(self.device)
 
