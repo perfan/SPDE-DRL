3bcf5b7d1f7d3b44376ae452d6d3b2ad8e6dba4e
farnammn <41007224+farnammn@users.noreply.github.com>
2021-04-04 23:03:16 -0400

    Update SpdeEnv.py

--- SpdeEnv.py
+++ SpdeEnv.py
@@ -12,23 +12,32 @@ class SpdeEnv(gym.Env):
         'video.frames_per_second': 30
     }
     
-    def __init__(self, burgers, u_max, f_max, nu, eps, u_star):
+    @staticmethod
+    def sigmoid(x):
+        return 1/(1 + np.exp(-x))
+
+    def function_theta(self, theta):
+        a = np.arange(self.n_x, dtype = np.float32)
+        return np.tanh(theta[0] * a + theta[1]) * theta[2] + theta[3]
+    
+    def __init__(self, burgers, u_max, theta_high, nu, eps, u_star, theta_size, lambda1 = 0.05):
         self.burgers = burgers
         self.u_max = u_max
-        self.f_max = f_max
+        self.theta_high = theta_high
         self.n_x = self.burgers.NX
         self.x_max  = self.burgers.XMAX
         self.nu = nu
         self.eps = eps
         self.u_star = u_star 
         self.viewer = None
+        self.lambda1 = lambda1
+        self.theta_size = theta_size
         
-    
-        f_high = np.full((self.n_x), self.f_max, dtype = np.float32)
+  
         u_high = np.full((self.n_x), self.u_max, dtype = np.float32)
         self.action_space = spaces.Box(
-            low=-f_high,
-            high=f_high,
+            low=-self.theta_high,
+            high=self.theta_high,
             dtype=np.float32
         )
         self.observation_space = spaces.Box(
@@ -43,19 +52,22 @@ class SpdeEnv(gym.Env):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
 
-    def step(self, f, t_start, t_end):
+    def step(self, theta, t_start, t_end):
         u = self.state 
 
-        f = np.clip(f, -self.f_max, self.f_max)
+        theta = np.clip(theta, -self.theta_high, self.theta_high)
+        f = self.function_theta(theta)
         
         # costs = np.sum((u[:, -1] - self.u_star)**2)
         # costs = np.sum((u[:, -1] - self.u_star)**2)
         du = utils.differentiate(u[:,-1], self.x_max, self.n_x)
         
-        #u_avg  = np.average(u[:,-1])
-        #costs = np.average((u[:, -1] -  u_avg)**2)
+        regulizer = self.lambda1 * np.average((f[1:] - f[:-1])**2)
+        u_avg  = np.average(u[:,-1])
+        costs = np.average((u[:, -1] -  u_avg)**2)
+        # costs += regulizer
 
-        costs = np.max(u[:,-1]) - np.min(u[:,-1])
+        # costs = np.max(u[:,-1]) - np.min(u[:,-1])
         self.state = self.burgers.convection_diffusion(t_start, t_end, self.nu, self.eps, u, f)
         
 
