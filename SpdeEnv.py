2deb5ce3c8c1ec5a5bdf2d05655419870ccb7bf4
farnammn <41007224+farnammn@users.noreply.github.com>
2021-04-08 21:36:42 -0400

    Update SpdeEnv.py

--- SpdeEnv.py
+++ SpdeEnv.py
@@ -16,34 +16,40 @@ class SpdeEnv(gym.Env):
     def sigmoid(x):
         return 1/(1 + np.exp(-x))
 
-    def function_theta(self, theta):
-        a = np.arange(self.n_x, dtype = np.float32)
+    def tanh_linear_comp(self, theta):
+        config = self.config
+        theta = np.clip(theta, -config.theta_high, config.theta_high)
+        a = np.arange(config.n_x, dtype = np.float32)
         f = np.tanh(theta[0] * a + theta[1]) * theta[2] + theta[3]
-        return f - np.average(f)
+        return f
     
-    def __init__(self, burgers, u_max, f_max, theta_high, theta_size, nu, eps, u_star, lambda1 = 0.05):
+    def piece_wise_constant(self, theta):
+        config = self.config
+        theta_size = len(theta)
+        theta = np.clip(theta, -config.theta_high, config.theta_high)
+        f = np.array([], dtype = np.float32)
+        for i in range(config.theta_size):
+          f = np.concatenate([f, np.full((config.n_x // theta_size),  theta[i], dtype = np.float32)])
+        f = np.concatenate([f, np.full((config.n_x % theta_size),  theta[theta_size - 1], dtype = np.float32)])
+        return f
+
+    def regularize_f(self, f):
+        f = np.clip(f, -self.config.f_max, self.config.f_max)
+        return f - np.average(f)
+
+    def __init__(self, burgers, config):
         self.burgers = burgers
-        self.u_max = u_max
-        self.theta_high = theta_high
-        self.f_max = f_max
-        self.n_x = self.burgers.NX
-        self.x_max  = self.burgers.XMAX
-        self.nu = nu
-        self.eps = eps
-        self.u_star = u_star 
-        self.viewer = None
-        self.lambda1 = lambda1
-        self.theta_size = theta_size
+        self.config = config
         
-  
-        u_high = np.full((self.n_x), self.u_max, dtype = np.float32)
-        # f_high = np.full((self.n_x), self.f_max, dtype = np.float32)
+      
+        u_high = np.full((config.n_x), config.u_max, dtype = np.float32)
 
         self.action_space = spaces.Box(
-            low=-theta_high,
-            high=theta_high,
+            low=-config.theta_high,
+            high=config.theta_high,
             dtype=np.float32
         )
+
         self.observation_space = spaces.Box(
             low=-u_high,
             high=u_high,
@@ -57,18 +63,23 @@ class SpdeEnv(gym.Env):
         return [seed]
 
     def step(self, theta, t_start, t_end, prev_condition):
-
-        theta = np.clip(theta, -self.theta_high, self.theta_high)
-        f = self.function_theta(theta)
-        f = np.clip(f, -self.f_max, self.f_max)
+        config = self.config
+        if config.func_type == "piece-wise-constant":
+            f = self.piece_wise_constant(theta)
+        elif config..func_type == "linear-tanh":
+            f = self.tanh_linear_comp(theta)
+        elif config.func_type == "linear":
+            f = theta
+        f = self.regularize_f(f)
         
-        u = self.burgers.convection_diffusion(t_start, t_end, self.nu, self.eps, prev_condition, f)
+        
+        u = self.burgers.convection_diffusion(t_start, t_end, config.nu, config.eps, prev_condition, f)
         self.state = u
         # costs = np.sum((u[:, -1] - self.u_star)**2)
         # costs = np.sum((u[:, -1] - self.u_star)**2)
-        du = utils.differentiate(u[:,-1], self.x_max, self.n_x)
+        du = utils.differentiate(u[:,-1], config.x_max, config.n_x)
         
-        regularizer = self.lambda1 * np.average((f[1:] - f[:-1])**2)
+        regularizer = config.regularizer_weight * np.average((f[1:] - f[:-1])**2)
         u_avg  = np.average(u[:,-1])
         costs = np.average((u[:, -1] -  u_avg)**2)
         # costs += regularizer
@@ -78,6 +89,6 @@ class SpdeEnv(gym.Env):
 
     def reset(self):
         self.state = self.burgers.u 
-        self.state[:, 0] = self.burgers.InitialCondition(self.nu) 
+        self.state[:, 0] = self.burgers.InitialCondition(config.nu) 
         self.last_u = None
         return self.state[:, 0]
