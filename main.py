e22070959c589173c68a1edd345ccbc8e8c8a260
faraz20230 <faraz.khoshbakhtian@mail.utoronto.ca>
2021-04-11 17:06:11 +0000

    sunday meeting

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ config.n_x = 151
 config.x_max = 2.0 * PI
 config.nu = 0.01
 config.eps = 0.01 #0.01
-config.regularizer_weight = 0.2
+config.regularizer_weight = 0
 config.func_type = "piece-wise-constant"
 config.cost_type = "variance"
 config.u_max = 8
@@ -35,92 +35,111 @@ config.tau = 0.1
 seed = 0
 np.random.seed(seed)
 
-burgers = Burgers(config.x_max, config.n_x, config.n_t)
+FUNC_TYPE = ['linear-tanh', 'linear', 'piece-wise-constant', 'piece-wise-constant', 'piece-wise-constant', 'piece-wise-constant']
+
+THETA_SIZE = [0, 0, 4, 6, 10, 15]
+
+for f_t, t_s in zip(FUNC_TYPE, THETA_SIZE):
+    burgers = Burgers(config.x_max, config.n_x, config.n_t)
+
+    config.func_type = f_t
+    if config.func_type == "piece-wise-constant":
+        config.theta_size = t_s
+        config.theta_high = np.full((config.theta_size), config.f_max, dtype = np.float32)
+        config.theta_scale = np.full((config.theta_size), 1)
+    elif config.func_type == "linear-tanh":
+        config.theta_high = np.array([1, config.n_x, config.f_max, config.f_max], dtype = np.float32)
+        config.theta_scale = [1, 50, 1, 1]
+        config.theta_size = 4
+    else:
+        config.theta_high = np.full((config.n_x), config.f_max, dtype = np.float32)
+        config.theta_scale = np.full((config.n_x), 1,  dtype = np.float32)
+        config.theta_size = config.n_x
 
 
-if config.func_type == "piece-wise-constant":
-    config.theta_size = 10
-    config.theta_high = np.full((config.theta_size), config.f_max, dtype = np.float32)
-    config.theta_scale = np.full((config.theta_size), 1)
-elif config.func_type == "linear-tanh":
-    config.theta_high = np.array([1, config.n_x, config.f_max, config.f_max], dtype = np.float32)
-    config.theta_scale = [1, 50, 1, 1]
-    config.theta_size = 4
-else:
-    config.theta_high = np.full((config.n_x), config.f_max, dtype = np.float32)
-    config.theta_scale = np.full((config.n_x), 1,  dtype = np.float32)
-    config.theta_size = config.n_x
+
+    env = SpdeEnv(burgers, config)
+    chkpt_dir = 'experiment_out'
+    make_dir(chkpt_dir)
+
+    agent = Agent(alpha= config.alpha, beta=config.beta, input_dims=[config.n_x], tau=config.tau, env=env,
+                  batch_size= config.batch_size,  layer1_size=config.layer1_size, layer2_size=config.layer2_size, n_actions= config.theta_size, chkpt_dir=chkpt_dir)
 
 
-
-env = SpdeEnv(burgers, config)
-chkpt_dir = 'experiment_out'
-make_dir(chkpt_dir)
-
-agent = Agent(alpha= config.alpha, beta=config.beta, input_dims=[config.n_x], tau=config.tau, env=env,
-              batch_size= config.batch_size,  layer1_size=config.layer1_size, layer2_size=config.layer2_size, n_actions= config.theta_size, chkpt_dir=chkpt_dir)
-
-
-make_dir('logs')
-now = datetime.now()
-current_time = now.strftime("%H:%M:%S")
-log_dir_name = "./logs/{}-{}-alpha{}-beta{}".format(current_time, config.func_type, config.alpha, config.beta)
-os.mkdir(log_dir_name)
-
-score_history = []
-
-timeStep = (config.t_end - config.t_start) / (config.episode_length-1)
-for j in range(config.num_episodes):
-    obs = env.reset()
-    done = False
-    score = 0
-
+    make_dir('logs')
     now = datetime.now()
-    iter_log_dir_name = "{}/{}".format(log_dir_name, j)
-    os.mkdir(iter_log_dir_name)
-    for i in range(config.episode_length):
-        t_init = i * timeStep
-        t_final = (i + 1) * timeStep
+    current_time = now.strftime("%H:%M:%S")
+    log_dir_name = "./logs/{}-{}-alpha{}-beta{}".format(current_time, config.func_type, config.alpha, config.beta)
+    os.mkdir(log_dir_name)
 
-        act = agent.choose_action(obs).astype('double')
-        act = act * config.theta_scale
-        
-        idx = np.argmax(obs)
-        
-        if i%100 == 0 :
-            if config.func_type == "piece-wise-constant":
-                f = env.piece_wise_constant(act)
-            elif config.func_type == "linear-tanh":
-                f = env.tanh_linear_comp(act)
-            else:
-                f = act
-            f = env.regularize_f(f)
+    score_history = []
+    best_score_history = []
+    best_iteration_history = []
 
-            plt.plot(f)
-            plt.savefig("{}/{}-act.png".format(iter_log_dir_name, i))
-            plt.close()
+    timeStep = (config.t_end - config.t_start) / (config.episode_length-1)
+    for j in range(config.num_episodes):
+        obs = env.reset()
+        done = False
+        score = 0
 
-            plt.plot(obs)
-            plt.savefig("{}/{}-obs.png".format(iter_log_dir_name, i))
-            plt.close()
+        now = datetime.now()
+        iter_log_dir_name = "{}/{}".format(log_dir_name, j)
+        os.mkdir(iter_log_dir_name)
+        for i in range(config.episode_length):
+            t_init = i * timeStep
+            t_final = (i + 1) * timeStep
 
-        new_state, derivaties, reward, done, info = env.step(act, t_init, t_final, obs)
-        
-        if i%100 == 0 :
-            plt.plot(new_state)
-            plt.savefig("{}/{}-newState.png".format(iter_log_dir_name, i))
-            plt.close()
+            act = agent.choose_action(obs).astype('double')
+            act = act * config.theta_scale
+            
+            idx = np.argmax(obs)
+            
+            if i%100 == 0 :
+                if config.func_type == "piece-wise-constant":
+                    f = env.piece_wise_constant(act)
+                elif config.func_type == "linear-tanh":
+                    f = env.tanh_linear_comp(act)
+                else:
+                    f = act
+                f = env.regularize_f(f)
 
-        agent.remember(obs, act, reward, new_state, int(done))
-        agent.learn()
-        score += reward
-        obs = new_state
+                plt.plot(f)
+                plt.savefig("{}/{}-act.png".format(iter_log_dir_name, i))
+                plt.close()
 
-    score = score / config.episode_length
-    score_history.append(score)
+                plt.plot(obs)
+                plt.savefig("{}/{}-obs.png".format(iter_log_dir_name, i))
+                plt.close()
 
-    print('episode ', j, 'score %.2f' % score,
-          'trailing 100 games avg %.3f' % np.mean(score_history[-100:]))
+            new_state, derivaties, reward, done, info = env.step(act, t_init, t_final, obs)
+            
+            if i%100 == 0 :
+                plt.plot(new_state)
+                plt.savefig("{}/{}-newState.png".format(iter_log_dir_name, i))
+                plt.close()
 
-filename = '{}/returns.png'.format(log_dir_name)
-plotLearning(score_history, filename, window=100)
+            agent.remember(obs, act, reward, new_state, int(done))
+            agent.learn()
+            score += reward
+            obs = new_state
+
+        score = score / config.episode_length
+        score_history.append(score)
+
+        best_score = np.max(score_history)
+        best_iteration = np.argmax(score_history)
+
+        best_score_history.append(best_score)
+        best_iteration_history.append(best_iteration)
+
+        print('episode ', j, 'score %.2f' % score,
+              'trailing 100 games avg %.3f' % np.mean(score_history[-100:]))
+
+    filename = '{}/returns.png'.format(log_dir_name)
+    plotLearning(score_history, filename, window=100)
+
+    filename = '{}/best_scores.png'.format(log_dir_name)
+    plotLearning(best_score_history, filename, window=100)
+
+    filename = '{}/best_iteration.png'.format(log_dir_name)
+    plotLearning(best_iteration_history, filename, window=100)
