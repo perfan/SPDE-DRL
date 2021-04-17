046e8dfd04fd56c704b3288d52ce96a44ef4c61e
perfan <56326009+perfan@users.noreply.github.com>
2021-04-17 15:02:32 -0400

    Burgers solver modified to be consistent with config()

--- BurgerSolver.py
+++ BurgerSolver.py
@@ -3,35 +3,39 @@ import numpy as np
 from SpdeEnv import SpdeEnv
 from SPDEs import Burgers
 from math import pi as PI
+from utils import *
 
 
 
-NT = 1201
-T_START = 0
-T_END = 2
-NX = 151
-XMAX = 2.0*PI
-NU = 0.01
-EPS = 0.00 # 0.01
-seed  = 0
+config = Config()
+config.n_t = 1201
+config.t_start = 0
+config.t_end = 2
+config.n_x = 151
+config.x_max = 2.0 * PI
+config.nu = 0.01
+config.eps = 0.01
+config.regularizer_weight = 0
+config.func_type = "linear"
+config.cost_type = "variance"
+config.u_max = 8
+config.f_max = 10
 
-np.random.seed(seed)
+np.random.seed(0)
 
-burgers = Burgers(XMAX, NX, NT)
-UMAX = 8
-USTAR = np.full(NX, 0, dtype = np.float32)
-FMAX = 10
-F_HIGH = np.full((NX), FMAX, dtype = np.float32)
+burgers = Burgers(config.x_max, config.n_x, config.n_t)
 
-f_control = np.concatenate([ np.full((75), -2),  np.full((76), 2)])
+config.theta_high = np.full((151), config.f_max, dtype = np.float32)
+# f_control = np.concatenate([ np.full((75), -2),  np.full((76), 2)])
+f_control = np.full((151), 0)
 print(f_control)
 
 
 
-env = SpdeEnv(burgers, UMAX, FMAX, F_HIGH, NU, EPS, USTAR, func_type = "linear")
+env = SpdeEnv(burgers, config)
 prev_condition = env.reset()
 
-u, du, reward, done, _ = env.step(f_control, T_START, T_END, prev_condition)
+u, du, reward, done, _ = env.step(f_control, config.t_start, config.t_end, prev_condition)
 
 print(reward)
 
@@ -46,13 +50,14 @@ plt.ylabel(r'$u$', fontsize = 10)
 plt.xticks(fontsize=10)
 plt.yticks(fontsize=10)  
 
-plt.plot(burgers.x, env.state[:,0], label="u_initial", color="black", lw=2)
-plt.plot(burgers.x, env.state[:,200], label="u_t1", color="red", lw=2)
-plt.plot(burgers.x, env.state[:,400], label="u_t2", color="green", lw=2)
-plt.plot(burgers.x, env.state[:,-1], label="u_end", color="blue", lw=2)
+plt.plot(burgers.x, env.state[:,0], label=r'$t=0$(s)', color="black", lw=2)
+plt.plot(burgers.x, env.state[:,200], label=r'$t=16$(s)', color="red", lw=2)
+plt.plot(burgers.x, env.state[:,400], label=r'$t=0.33$(s)', color="green", lw=2)
+plt.plot(burgers.x, env.state[:,600], label=r'$t=0.50$(s)', color="blue", lw=2)
 
-plt.xlim(0, XMAX)
+plt.xlim(0, config.x_max)
 # plt.ylim(0, 7.5)
 plt.legend(prop={'size': 10})
 plt.minorticks_on()    
 plt.savefig("test-some-control-2")
+plt.show()
